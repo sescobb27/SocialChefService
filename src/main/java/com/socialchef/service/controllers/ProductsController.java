264c00bcfc39bce928d1a6d14ce6df0928d13e6d
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-03-22 00:18:35 -0500

    Product search by name, category, chef and location

--- src/main/java/com/socialchef/service/controllers/ProductsController.java
+++ src/main/java/com/socialchef/service/controllers/ProductsController.java
@@ -5,10 +5,10 @@ import java.util.LinkedList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.socialchef.service.models.Product;
-import com.socialchef.service.models.User;
 
 @Controller
 public class ProductsController {
@@ -18,26 +18,30 @@ public class ProductsController {
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     @ResponseBody
     public LinkedList<Product> getAllProducts() {
-	mocks = new LinkedList<Product>();
-
-	for (int i = 0; i < Product.names.length; ++i) {
-	    String u_name = User.names[i];
-	    String u_last_name = User.last_names[i];
-	    String u_username = User.usernames[i];
-	    String u_email = User.emails[i];
-
-	    User u = new User(u_name, u_last_name, u_username, u_email, "");
-	    String p_name = Product.names[i];
-	    String p_category = Product.categories[i];
-	    String p_location = Product.locations[i];
-	    String p_description = Product.descriptions[i];
-	    float p_price = Product.prices[i];
-
-	    Product p = new Product(p_name, p_category, p_location,
-		    p_description, "", u, p_price, null, null);
-	    mocks.add(p);
-	}
-
+	mocks = Product.mockProducts();
 	return mocks;
     }
+
+    @RequestMapping(value = "/products/findby", method = RequestMethod.GET)
+    @ResponseBody
+    public LinkedList<Product> findBy(
+	    @RequestParam(value = "key", required = false, defaultValue = "match") String key,
+	    @RequestParam(value = "search_value", required = false, defaultValue = "adds") String search_value) {
+	System.out.println("key: " + key);
+	System.out.println("search: " + search_value);
+	switch (key) {
+	case "name":
+	    return Product.findByName(search_value);
+	case "category":
+	    return Product.findByCategory(search_value);
+	case "location":
+	    return Product.findByPrice(search_value);
+	case "price":
+	    return Product.findByLocation(search_value);
+	default:
+	    // TODO
+	    return null;
+	}
+    }
+
 }
--- src/main/java/com/socialchef/service/models/Product.java
+++ src/main/java/com/socialchef/service/models/Product.java
@@ -1,6 +1,7 @@
 package com.socialchef.service.models;
 
 import java.util.Date;
+import java.util.LinkedList;
 
 public class Product {
 
@@ -9,14 +10,15 @@ public class Product {
     private float rate = 0.0f;
     private double price;
     private Date delivery_time, disponibility_time;
-// TESTING
+    // TESTING
     public static String[] names = { "Plato1", "Plato2", "Plato3" };
     public static String[] categories = { "pastas", "carne", "ensalada" };
     public static String[] locations = { "Poblado", "Laureles", "Envigado" };
     public static String[] descriptions = { "Descripcion1", "Descripcion2",
-        "Descripcion3" };
+	    "Descripcion3" };
     public static float[] prices = { 18500.0f, 12300.0f, 5000.0f };
-// END TESTING
+
+    // END TESTING
     public Product(String name, String category, String location,
 	    String description, String image, User chef, double price,
 	    Date delivery_time, Date disponibility_time) {
@@ -109,6 +111,65 @@ public class Product {
 
     public void setDisponibility_time(Date disponibility_time) {
 	this.disponibility_time = disponibility_time;
+    }
+
+    public static LinkedList<Product> mockProducts() {
+	LinkedList<Product> mocks = new LinkedList<Product>();
+	for (int i = 0; i < Product.names.length; ++i) {
+	    String u_name = User.names[i];
+	    String u_last_name = User.last_names[i];
+	    String u_username = User.usernames[i];
+	    String u_email = User.emails[i];
+
+	    User u = new User(u_name, u_last_name, u_username, u_email, "");
+	    String p_name = Product.names[i];
+	    String p_category = Product.categories[i];
+	    String p_location = Product.locations[i];
+	    String p_description = Product.descriptions[i];
+	    float p_price = Product.prices[i];
+
+	    Product p = new Product(p_name, p_category, p_location,
+		    p_description, "", u, p_price, null, null);
+	    mocks.add(p);
+	}
+	return mocks;
+    }
+
+    private static LinkedList<Product> mockSearch(String query) {
+	LinkedList<Product> mocks = Product.mockProducts();
+	LinkedList<Product> results = new LinkedList<Product>();
+	for (Product p : mocks) {
+	    if (p.name.contains(query) || p.category.contains(query)
+		    || p.chef.getUsername().contains(query)
+		    || p.location.contains(query)) {
+		results.add(p);
+	    }
+	}
+	return results;
+    }
+
+    public static LinkedList<Product> findByName(String p_name) {
+	// TODO
+	// SEARCH QUERY
+	return mockSearch(p_name);
+    }
+
+    public static LinkedList<Product> findByCategory(String p_category) {
+	// TODO
+	// SEARCH QUERY
+	return mockSearch(p_category);
+    }
+
+    public static LinkedList<Product> findByPrice(String p_price) {
+	// TODO
+	// SEARCH QUERY
+	return mockSearch(p_price);
+    }
+
+    public static LinkedList<Product> findByLocation(String p_location) {
+	// TODO
+	// SEARCH QUERY
+	return mockSearch(p_location);
     }
 
 }
