96df4ca86a39e2409baa06dc17290efc4278002c
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-04-16 00:17:50 -0500

    ProductServiceRepository Autowire and HttpSession Autowire

--- src/main/java/com/socialchef/service/controllers/ProductsController.java
+++ src/main/java/com/socialchef/service/controllers/ProductsController.java
@@ -1,7 +1,11 @@
 package com.socialchef.service.controllers;
 
+import java.util.Set;
 import java.util.LinkedList;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.socialchef.service.models.Product;
+import com.socialchef.service.repositories.implementation.ProductServiceRepository;
 
 @Controller
 public class ProductsController {
+
+	@Autowired
+	private ProductServiceRepository productRepo;
+	@Autowired
+	private HttpSession session;
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     @ResponseBody
@@ -20,27 +30,32 @@ public class ProductsController {
 //    	return mocks;
     	return null;
     }
-    
+
     // /products/findby?key=category&search_value=carne
     @RequestMapping(value = "/products/findby", method = RequestMethod.GET)
     @ResponseBody
-    public LinkedList<Product> findBy(
+    public Set<Product> findBy(
 	    @RequestParam(value = "key", required = false, defaultValue = "") String key,
 	    @RequestParam(value = "search_value", required = false, defaultValue = "") String search_value) {
 		System.out.println("key: " + key);
 		System.out.println("search: " + search_value);
-		switch (key) {
-		case "name":
-		    return Product.findByName(search_value);
-		case "category":
-		    return Product.findByCategory(search_value);
-		case "location":
-		    return Product.findByLocation(search_value);
-		case "price":
-		    return Product.findByPrice(search_value);
-		default:
-		    return Product.findByRegex(search_value);
+		try {
+			switch (key) {
+			case "name":
+			    return productRepo.findByName(search_value);
+			case "category":
+			    return productRepo.findByCategory(search_value);
+			case "location":
+			    return productRepo.findByLocation(search_value);
+			case "price":
+			    return productRepo.findByPrice(Float.parseFloat(search_value));
+			default:
+			    return productRepo.findByRegex(search_value);
+			}
+		} catch (Exception ex) {
+
 		}
+		return null;
     }
 
 }
