3fa1424f59b6a86356a51b8cf0a1ce639fbcfcd3
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-04-17 01:08:47 -0500

    /chefs/addproduct method, Multipart is not working yet

--- src/main/java/com/socialchef/service/controllers/UsersController.java
+++ src/main/java/com/socialchef/service/controllers/UsersController.java
@@ -1,5 +1,9 @@
 package com.socialchef.service.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -7,13 +11,17 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.codec.binary.Base64;
 import org.bouncycastle.util.encoders.Hex;
+import org.hibernate.SessionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -22,11 +30,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.socialchef.service.exceptions.InvalidDataException;
+import com.socialchef.service.models.Product;
 import com.socialchef.service.models.User;
+import com.socialchef.service.repositories.implementation.ProductServiceRepository;
 import com.socialchef.service.repositories.implementation.UserServiceRepository;
 
 @Controller
@@ -35,7 +47,7 @@ public class UsersController {
 	@Autowired
 	private UserServiceRepository userRepo;
 	@Autowired
-	private HttpSession session;
+	private ProductServiceRepository productRepo;
 	
 	@RequestMapping(value = "/chefs", method = RequestMethod.GET)
 	@ResponseBody
@@ -52,7 +64,8 @@ public class UsersController {
 	@RequestMapping(value="/chefs", method=RequestMethod.POST)
 	@ResponseStatus(HttpStatus.NO_CONTENT) // 204
 	public void createNewUser(@RequestBody Map<String, String> body,
-			HttpServletResponse response, HttpServletRequest request) {
+			HttpServletResponse response, HttpServletRequest request,
+			HttpSession session) {
 		String name = body.get("name");
 		String lastName = body.get("lastName");
 		String userName = body.get("userName");
@@ -74,6 +87,77 @@ public class UsersController {
 		}else if (user.hasErrors()) {
 			throw new InvalidDataException(user.getErrors());
 		}
+	}
+	
+	@RequestMapping(value="/chefs/addproduct", method=RequestMethod.POST)
+	@ResponseBody
+	public void addProduct(HttpSession session,
+			@CookieValue(value="session_id", 
+				defaultValue="", required=true) String session_id,
+			@RequestParam(value = "username",
+				required = true, defaultValue = "") String username,
+			@RequestParam(value = "productname",
+				required = true, defaultValue = "") String productname,
+			@RequestParam(value = "price",
+				required = true, defaultValue = "0.0") String price,
+			@RequestParam(value = "image") MultipartFile image) {
+		
+		String imagePath;
+		if( image != null && !image.isEmpty() )
+			imagePath = saveImage(image);
+//		synchronized (session) {
+//			username = (String) session.getAttribute(session_id);
+//			if( username == null || username.length() == 0 ||
+//					uname == null || !username.equalsIgnoreCase(uname) ) {
+//				throw new SessionException("Necesitas iniciar sesion para agregar un producto");
+//			}
+//		}
+		Product p = new Product(productname, "", Double.parseDouble(price));
+		p.setUser(userRepo.findByUsername(username));
+		if ( productRepo.create(p) )
+			throw new InvalidDataException(p.getErrors());
+	}
+	
+	private String saveImage(MultipartFile image) {
+		MessageDigest md;
+		String name;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			Timestamp now = new Timestamp(new Date().getTime());
+			md.update(image.getName().getBytes("UTF-8"));
+			md.update(now.toString().getBytes("UTF-8"));
+			byte[] digest = md.digest();
+			name =  new String(Hex.encode(digest));
+			File file = new File(name+".jpg");
+//			try with resource
+			try ( FileOutputStream stream = new FileOutputStream(file) ) {
+				stream.write(image.getBytes());
+			}
+			String path = file.getPath();
+			String abPath = file.getAbsolutePath();
+			return file.getPath();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@RequestMapping(value="/chefs/listproducts", method=RequestMethod.GET)
+	public Set<Product> listProducts(@CookieValue(value="session_id",
+			defaultValue="", required=true) String session_id,
+			HttpSession session) {
+		String username;
+		synchronized (session) {
+			username = (String) session.getAttribute(session_id);
+			if( username == null || username.length() == 0 ) {
+				throw new SessionException("Necesitas iniciar sesion para agregar un producto");
+			}
+		}
+		return productRepo.findByUserName(username);
 	}
 	
 	private String makeSessionId(HttpServletRequest request) {
