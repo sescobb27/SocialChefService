9ffbaaa706ff06f1b167d267699a2ebfe5257447
jmejiaz <jmejiaz@eafit.edu.co>
2014-04-24 16:37:26 -0500

    testing my fork

--- src/main/java/com/socialchef/service/controllers/IndexController.java
+++ src/main/java/com/socialchef/service/controllers/IndexController.java
@@ -14,6 +14,7 @@ import com.socialchef.service.models.Category;
 import com.socialchef.service.models.Location;
 import com.socialchef.service.repositories.implementation.ValuesServiceRepository;
 
+//testing my fork
 @Controller
 public class IndexController {
 
