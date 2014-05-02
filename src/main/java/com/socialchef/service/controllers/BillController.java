34f8e25b615822c958affabea02065ea924cb948
jmejiaz <jmejiaz@eafit.edu.co>
2014-05-01 22:54:24 -0500

    first bill.pdf ready localhost:8080/bill

--- src/main/java/com/socialchef/service/controllers/BillController.java
+++ src/main/java/com/socialchef/service/controllers/BillController.java
@@ -11,6 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilePermission;
 
 import com.socialchef.service.models.Bill;
 import com.socialchef.service.repositories.implementation.BillServiceRepository;
@@ -27,12 +34,12 @@ public class BillController {
 	@RequestMapping(value = "/bill", method = RequestMethod.GET)
 	@ResponseBody
 	public List<List<Bill>> getAllProducts() {
-
+		
 		List<Bill> listBills = BillRepo.selectAll();
 
 		int iterator = 0;
 
-		chef_id= (listBills.get(iterator).getChef_id());
+		chef_id = (listBills.get(iterator).getChef_id());
 
 		for (Bill bill : listBills) {
 
@@ -40,22 +47,100 @@ public class BillController {
 
 				userListBill.add(bill);
 				iterator++;
-				
+
 				continue;
 			} else {
 				usersListBills.add(userListBill);
 				this.userListBill = new ArrayList<Bill>();
 				userListBill.add(bill);
-				chef_id= (listBills.get(iterator).getChef_id());
+				chef_id = (listBills.get(iterator).getChef_id());
 				continue;
-				//iterator++;
+
 			}
 
-			
-
 		}
+		printer(usersListBills.get(0));
 		return usersListBills;
 
+	}
+	
+	public void printer(List<Bill> userBill ){
+		
+		Document document = new Document();
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream("prueba numero 1.pdf"));
+			document.open();
+			Paragraph paragraph  = new Paragraph();
+			paragraph.add("esto es la verga");
+			document.add(paragraph);
+			
+            PdfPTable table = new PdfPTable(6); // 6 columns.
+            table.setSpacingBefore(25f);
+            table.setSpacingAfter(25f); 
+            table = setColumnHeader(table);            
+            table = addRows(table,userBill);
+            document.add(table);
+			document.close();
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public PdfPTable setColumnHeader(PdfPTable table){
+		
+        PdfPCell cell1 = new PdfPCell(new Phrase("Nro de transaccion"));
+        cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1.setBackgroundColor(BaseColor.GRAY);
+        PdfPCell cell2 = new PdfPCell(new Phrase("Fecha"));
+        cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell2.setBackgroundColor(BaseColor.GRAY);
+        PdfPCell cell3 = new PdfPCell(new Phrase("Nombre del producto"));
+        cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell3.setBackgroundColor(BaseColor.GRAY);
+        PdfPCell cell4 = new PdfPCell(new Phrase("Cliente"));
+        cell4.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell4.setBackgroundColor(BaseColor.GRAY);
+        PdfPCell cell5 = new PdfPCell(new Phrase("Costo total"));
+        cell5.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell5.setBackgroundColor(BaseColor.GRAY);
+        PdfPCell cell6 = new PdfPCell(new Phrase("Comision"));
+        cell6.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell6.setBackgroundColor(BaseColor.GRAY);
+        table.addCell(cell1);
+        table.addCell(cell2);
+        table.addCell(cell3);
+        table.addCell(cell4);
+        table.addCell(cell5);
+        table.addCell(cell6);
+		
+		return table;
+		
+		
+	}
+	
+	public PdfPTable addRows(PdfPTable table,List<Bill> userBill ){
+     	 for(int x = 0; x<userBill.size();x++){
+    		 
+    		 PdfPCell cell1 = new PdfPCell(new Phrase(userBill.get(x).getTransaction_id().toString()));
+    		 table.addCell(cell1);
+    		 PdfPCell cell2 = new PdfPCell(new Phrase(userBill.get(x).getDate().toString()));
+    		 table.addCell(cell2);
+    		 PdfPCell cell3 = new PdfPCell(new Phrase(userBill.get(x).getProductName()));
+    		 table.addCell(cell3);
+    		 PdfPCell cell4 = new PdfPCell(new Phrase(userBill.get(x).getCustomerName()));
+    		 table.addCell(cell4);
+    		 PdfPCell cell5 = new PdfPCell(new Phrase(userBill.get(x).getTotalCost().toString()));
+    		 table.addCell(cell5);
+    		 PdfPCell cell6 = new PdfPCell(new Phrase(userBill.get(x).getComission().toString()));
+    		 table.addCell(cell6);
+    	 }
+     	 return table;
+		
 	}
 
 	public Integer getChef_id() {
